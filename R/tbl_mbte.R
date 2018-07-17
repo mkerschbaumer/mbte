fad804677ef8de40159772838969bb7fb7541650
Martin Kerschbaumer <maff32@gmx.com>
2018-07-17 10:43:58 +0200

    introduce tbl_mbte()

---
+++ R/tbl_mbte.R
@@ -0,0 +1,46 @@
+#' Creates a new dataset, suited for the the signal detection workflow
+#'
+#' A function intended to be used internally or to extend mbte.
+#'
+#' @param x A data.frame or tibble
+#' @param time A symbol for the time-column (gets quoted). The corresponding
+#' column must be present in the dataset.
+#' @param value The value column (measurement-data); gets quoted; The
+#' corresponding column must be present in the dataset.
+#' @param signal A name for a list column, in which tibbles containing
+#' the signal (time- and value- column combined) get stored; gets quoted
+#' @param fits List-column-name; The predicted values for the original signal
+#' get stored in this list-column.
+#' @param metric The name for numeric-column containing the computed
+#' error-metric.
+#' @param ... Additional attributes, that are set.
+#'
+#' @importFrom assertthat assert_that
+#' @importFrom rlang ensym
+#' @importFrom tibble new_tibble
+#' @export
+new_tbl_mbte <- function(x, time, value, ..., signal = "signal", fits = "fits",
+                         metric = "metric", subclass = NULL) {
+  time <- ensym(time)
+  value <- ensym(value)
+  signal <- ensym(signal)
+  fits <- ensym(fits)
+  metric <- ensym(metric)
+
+  assert_column_in_dataset(!!time, x, substitute(x))
+  assert_column_in_dataset(!!value, x, substitute(x))
+  assert_column_not_in_dataset(!!signal, x, substitute(x))
+  assert_column_not_in_dataset(!!fits, x, substitute(x))
+  assert_column_not_in_dataset(!!metric, x, substitute(x))
+
+  new_tibble(
+    x,
+    time = time,
+    value = value,
+    signal = signal,
+    fits = fits,
+    metric = metric,
+    ...,
+    subclass = c(subclass, "tbl_mbte")
+  )
+}
