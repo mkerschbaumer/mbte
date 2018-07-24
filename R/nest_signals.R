0a3af23ceae73015e58fb1b5f8b409201308d4ef
Martin Kerschbaumer <maff32@gmx.com>
2018-07-24 11:40:16 +0200

    error handling: adapt mbte_nest_signals()

--- R/nest_signals.R
+++ R/nest_signals.R
@@ -1,28 +1,34 @@
 #' Combine time- and variable- column to a list-column (signal).
 #'
-#' @param x A \code{tbl_mbte}, where the signals haven't been nested in a list
-#' column yet (contains a time-, value- and a grouping column).
+#' @param x A \code{tbl_mbte}, the time- and value-columns are still present (
+#' specified when creating the `tbl_mbte`).
 #' @param ... Variables (get quoted), which should be used for grouping. If no
 #' variables are specified, \code{\link{mbte_nest_signals}} assumes, that
 #' \code{x} is already a grouped table.
 #'
-#' @importFrom assertthat assert_that
-#' @importFrom dplyr group_by is_grouped_df
+#' @importFrom dplyr group_by
+#' @importFrom purrr walk
 #' @importFrom tidyr nest
 #' @importFrom rlang ensyms
 #' @export
 mbte_nest_signals <- function(x, ...) {
-  assert_that(is_tbl_mbte(x))
+  assert_is_tbl_mbte(x)
 
   group_vars <- ensyms(...)
   if (length(group_vars) != 0) {
+    # make sure all grouping variables specified by the user are contained
+    # in the dataset
+    walk(group_vars, ~{
+      assert_column_in_dataset(x, colname = .x, "(missing grouping-variable)")
+    })
+
     # use grouping provided by ellipsis (higher priority than possibly
     # existing grouping of table)
     x <- x %>%
       group_by(!!!group_vars, add = FALSE) %>%
       mbte_reconstruct(x)
   } else {
-    assert_that(is_grouped_df(x))
+    assert_grouped(x)
   }
 
   # extract relevant symbols
@@ -30,7 +36,12 @@ mbte_nest_signals <- function(x, ...) {
   value <- attr_value(x)
   signal <- attr_signal(x)
 
-  # combine time- and value- variable into signal-list-column
+  assert_has_column(x, time, "(time-column)")
+  assert_valid_time_col(x, time)
+  assert_has_column(x, value, "(value-column)")
+  assert_valid_value_col(x, value)
+
+  # combine time- and value- columns into "signal"-list-column
   x %>%
     nest(!!time, !!value, .key = !!signal) %>%
     mbte_reconstruct(x)
