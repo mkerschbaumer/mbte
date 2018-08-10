1442c807972ca367f7d9dcf09bdee8bf6655d7ee
Martin Kerschbaumer <maff32@gmx.com>
2018-08-10 11:48:59 +0200

    add mbte_unnest_fits()

--- R/unnest.R
+++ R/unnest.R
@@ -63,3 +63,90 @@ mbte_unnest_signals <- function(x) {
     select(!!!desc_cols, !!time, !!value) %>% # reorder columns
     mbte_reconstruct(x) # make sure a tbl_mbte gets returned
 }
+
+#' Perform unnesting of fitting data
+#'
+#' The `fits`-list column gets unnested. This function is useful if the fitted
+#' signal-values should be processed further.
+#'
+#' @return
+#' A \code{\link{tbl_mbte}} with the following columns:
+#' \describe{
+#'   \item{desc}{`desc` is a placeholder for all columns except the `signal`-
+#'     and the `fits`-columns. Those columns can be seen as "descriptive"
+#'     columns describing the measured signals.}
+#'   \item{fit}{The name of the method used for fitting (character). See
+#'     \code{\link{mbte_fit}} for details.}
+#'   \item{time}{The time-column unnested from the `signal`-column.}
+#'   \item{value}{The values of the predicted signals (originally in `signal`-
+#'     column).}
+#' }
+#'
+#' @inheritParams mbte_unnest_signals
+#'
+#' @seealso \code{\link{filtered_signals}} (dataset used in examples)
+#' @examples
+#' # load sample dataset
+#' data(filtered_signals)
+#' filtered_signals
+#'
+#' # perform fitting
+#' fitted <- mbte_fit(filtered_signals, lm = lm(value ~ t, .signal))
+#'
+#' # unnest predictions for signal-values (==> fitted signals)
+#' #
+#' # NOTE: "lm" is the only method used for fitting and therefore the `fit`-
+#' # column consists only of "lm"
+#' mbte_unnest_fits(fitted)
+#'
+#' @importFrom dplyr bind_cols mutate select
+#' @importFrom magrittr "%>%"
+#' @importFrom purrr pmap
+#' @importFrom rlang expr
+#' @importFrom tidyr gather unnest
+#' @family unnesting functions
+#' @export
+mbte_unnest_fits <- function(x) {
+  assert_is_tbl_mbte(x)
+
+  # extract column-name related symbols
+  time <- attr_time(x)
+  value <- attr_value(x)
+  signal <- attr_signal(x)
+  fits <- attr_fits(x)
+
+  # check integrity of columns
+  assert_has_column(x, signal, "(signal-column)")
+  assert_valid_signal_col(x, signal)
+  assert_has_column(x, fits, "(fits-column)")
+  assert_valid_fits_col(x, fits)
+
+  # add time column to `fits` and convert actual fits to long-form (column
+  # `fit` denotes the name of the fit)
+  .modified_fits <- list(x[[fits]], x[[signal]], seq_len(nrow(x))) %>%
+    pmap(function(fit, sig, ind) {
+      # generate expression of the form x$signal[[row_number]] (for better
+      # error-messages if assertions below fail)
+      x_sym <- expr(`[[`(`$`(x, !!signal), !!ind))
+
+      # check presence and integrity of time column
+      assert_has_column(sig, time, x_sym = x_sym, "- signal subtibble faulty")
+      assert_valid_time_col(sig, time, x_sym = x_sym)
+
+      # add time column; NOTE: `fit` becomes the column containing the names of
+      # the fits
+      bind_cols(fit, select(sig, !!time)) %>%
+        gather("fit", "value", -!!time)
+    })
+
+  # colnames of "descriptive" columns (columns not being signal- or
+  # fits-columns and not `fit` - reserved column name)
+  desc_cols <- setdiff(colnames(x), c(signal, fits, "fit"))
+
+  x %>%
+    # add time column to every table of `fits` list-column
+    mutate(!!fits := .modified_fits) %>%
+    unnest(!!fits) %>% # perform actual unnesting
+    select(!!!desc_cols, "fit", !!time, !!value) %>% # reorder columns
+    mbte_reconstruct(x) # make sure a tbl_mbte gets returned
+}
---
+++ tests/testthat/test-unnest_fits.R
@@ -0,0 +1,56 @@
+context("unnest_fits")
+
+# create datasets needed for testing
+raw_tbl_mbte <- new_tbl_mbte(raw_signals, "t", "value")
+fitted <- mbte_fit(filtered_signals, lm = lm(value ~ t, .signal))
+
+test_input_not_tbl_mbte(mbte_unnest_fits)
+
+# signal column not present or malformatted
+test_signal_col_np_mf(mbte_unnest_fits, raw_tbl_mbte)
+
+# fits column not present or malformatted
+test_fits_col_np_mf(mbte_unnest_fits, filtered_signals)
+
+# NOTE: only the time column of a signal is relevant; signal-values not needed
+# for mbte_unnest_fits()
+test_that("time column not present or malformatted", {
+  bad_signal <- fitted$signal[[3]]
+  stopifnot("t" %in% colnames(bad_signal)) # make sure time column is present
+
+  # malformat signal on purpose (time expected to be numeric, converted to
+  # character)
+  bad_signal$t <- as.character(bad_signal$t)
+  fitted$signal[[3]] <- bad_signal
+
+  # expect error describing where the malformatted signal can be found
+  expect_error(mbte_unnest_fits(fitted), "x$signal[[3L]]$t",
+    "err_class_mismatch", fixed = TRUE)
+
+  # "delete" time column
+  bad_signal$t <- NULL
+  fitted$signal[[3]] <- bad_signal
+
+  # expect error about column not found
+  expect_error(mbte_unnest_fits(fitted), "x$signal[[3L]]", "err_col_expected",
+    fixed = TRUE)
+})
+
+# no errors expected
+test_that("positive test", {
+  # expected result
+  exp <- fitted %>%
+    mutate(fits = map2(signal, fits, ~{
+      # add time column to fits and convert from wide form to long
+      gather(bind_cols(.y, .x["t"]), "fit", "value", -t)
+    })) %>%
+    unnest(fits) %>%
+    select(mv, signal_nr, fit, t, value) %>%
+    mbte_reconstruct(fitted)
+
+  # compute actual result
+  res <- mbte_unnest_fits(fitted)
+
+  # expect matching results
+  expect_tbl_mbte_equal(res, exp)
+})
