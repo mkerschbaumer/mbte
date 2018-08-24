c86684dc7754c920026248c0cb08232cf341d25a
Martin Kerschbaumer <maff32@gmx.com>
2018-08-24 10:54:41 +0200

    refactor tests of mbte_unnest_signals()

--- tests/testthat/test-unnest_signals.R
+++ tests/testthat/test-unnest_signals.R
@@ -1,7 +1,8 @@
 context("unnest_signals")
 
-raw_tbl_mbte <- new_tbl_mbte(raw_signals, "t", "value")
+raw_tbl_mbte <- gen_raw_tbl_mbte()
 
+# mbte_unnest_signals() should check integrity of input parameter
 test_input_not_tbl_mbte(mbte_unnest_signals)
 
 # test signal column not present or malformatted
@@ -9,21 +10,24 @@ test_signal_col_np_mf(mbte_unnest_signals, raw_tbl_mbte)
 
 # mbte_unnest_signals() should invert mbte_nest_signals()
 test_that("inversion of mbte_nest_signals", {
-  expect_silent(nested <- mbte_nest_signals(raw_tbl_mbte, mv))
-  expect_silent(unnested <- mbte_unnest_signals(nested))
+  # make sure unnesting also works if the signal variable is not named
+  # "signal" (default)
+  tbl <- new_tbl_mbte(raw_signals, t, value, signal = signal_var)
+
+  nested <- expect_silent(mbte_nest_signals(tbl, mv))
+  unnested <- expect_silent(mbte_unnest_signals(nested))
 
   # reorder columns (since mbte_unnest_signals() always puts time- and value-
   # columns after "descriptive" columns like `mv`)
-  reordered <- select(raw_tbl_mbte, mv, dplyr::everything())
+  reordered <- dplyr::select(tbl, mv, dplyr::everything())
 
   # make sure inversion has been successful
   expect_tbl_mbte_equal(unnested, reordered)
 })
 
 test_that("positive test with `fits`-column", {
-  # add `fits`-column
-  fitted <- filtered_signals %>%
-    mbte_fit(lm = lm(value ~ t, .signal))
+  # perform fitting (add `fits`-column)
+  fitted <- mbte_fit(filtered_signals, lm = lm(value ~ t, .signal))
 
   # unnesting of tbl_mbte with `fits`-column should yield the same results as
   # unnesting a tbl_mbte without `fits`-column
