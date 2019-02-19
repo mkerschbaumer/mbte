79be473f73f7f65d8210d946226fb3767344f4df
Martin Kerschbaumer <maff32@gmx.com>
2019-02-19 21:24:50 +0100

    refactor getting/setting of dataset-specific colnames to separate functions export refactored functions (for use in extension packages) use exact attribute matching

--- R/colnames.R
+++ R/colnames.R
@@ -7,6 +7,8 @@
 #'
 #' @param x A \code{\link{tbl_mbte}}.
 #' @param value The name of the column as a \code{\link[base:name]{symbol}}.
+#' @param name A character vector of length 1 (string) describing the purpose
+#'   of the column to get/set (e.g. \code{"signal"} or \code{"fits"}).
 #'
 #' @return Replacement functions like \code{colname_time<-} return a modified
 #'   \code{\link{tbl_mbte}}. Otherwise a \code{\link[base:name]{symbol}} is
@@ -16,52 +18,65 @@
 #' @name colname-functions
 NULL
 
+#' @describeIn colname-functions Set a specific column name. This function is
+#'   designed for extension-packages.
+#' @export
+#' @keywords internal
+colname_set <- function(x, value, name) {
+  stopifnot(is.character(name), length(name) == 1, is_symbol(value))
+  attr(x, name) <- value
+  x
+}
+
+#' @describeIn colname-functions Get a specific column name. This function is
+#'   designed for extension-packages.
+#' @export
+#' @keywords internal
+colname_get <- function(x, name) {
+  stopifnot(is.character(name), length(name) == 1)
+  attr(x, name, exact = TRUE)
+}
+
 #' @describeIn colname-functions Get column name for signal-time.
 #' @export
 #' @keywords internal
 colname_time <- function(x) {
-  attr(x, "time")
+  colname_get(x, "time")
 }
 
 #' @describeIn colname-functions Override column name for signal-time.
 #' @export
 #' @keywords internal
 `colname_time<-` <- function(x, value) {
-  stopifnot(is_symbol(value))
-  attr(x, "time") <- value
-  x
+  colname_set(x, value, "time")
 }
 
 #' @describeIn colname-functions Get column name for signal-values.
 #' @export
 #' @keywords internal
 colname_value <- function(x) {
-  attr(x, "value")
+  colname_get(x, "value")
 }
 
 #' @describeIn colname-functions Set column name for signal-values column.
 #' @export
 #' @keywords internal
 `colname_value<-` <- function(x, value) {
-  stopifnot(is_symbol(value))
-  attr(x, "value") <- value
-  x
+  colname_set(x, value, "value")
 }
 
 #' @describeIn colname-functions Get column name for signal-column.
 #' @export
 #' @keywords internal
 colname_signal <- function(x) {
-  attr(x, "signal")
+  colname_get(x, "signal")
 }
 
 #' @describeIn colname-functions Override column name for signal-column.
 #' @export
 #' @keywords internal
 `colname_signal<-` <- function(x, value) {
-  stopifnot(is_symbol(value))
-  attr(x, "signal") <- value
-  x
+  colname_set(x, value, "signal")
 }
 
 #' @describeIn colname-functions Get column name for fits-column (see
@@ -69,15 +84,13 @@ colname_signal <- function(x) {
 #' @export
 #' @keywords internal
 colname_fits <- function(x) {
-  attr(x, "fits")
+  colname_get(x, "fits")
 }
 
 #' @describeIn colname-functions Set column name for fits column.
 #' @export
 #' @keywords internal
 `colname_fits<-` <- function(x, value) {
-  stopifnot(is_symbol(value))
-  attr(x, "fits") <- value
-  x
+  colname_set(x, value, "fits")
 }
 
